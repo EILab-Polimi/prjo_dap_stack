4e1ee7961010c99852b1308331472c86eb2ede00
ilpise <pisewip@yahoo.it>
2022-04-21 11:43:36 +0200

    The stak fail compilation if drupal requirements are already sotisfied

--- services/drupal/setup/setup.sh
+++ services/drupal/setup/setup.sh
@@ -4,7 +4,12 @@
 echo "Running composer require"
 # composer --version
 # composer require drupal/group drupal/restui php-mqtt/client
-composer require 'drupal/bootstrap5:^1.1'
+
+# Required packages -- TODO remove if already resolved
+# composer require 'drupal/bootstrap5:^1.1'
+# composer require 'drupal/twig_tweak:^3.1'
+# http_client_manager  is not used right now
+# composer require 'drupal/http_client_manager:^2.5'
 
 # ./vendor/bin/drush en
 
